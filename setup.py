4b26a56782bcce5cdab32446a5320226a8600cb8
Tert0 <62036464+Tert0@users.noreply.github.com>
2021-02-24 22:07:25 +0100

    v1.0.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='quart_discord_oauth',
     packages=find_packages(),
-    version='1.0.7',
+    version='1.0.8',
     description='Discord OAuth Quart extension for APIs',
     author='Tert0',
     license='MIT',
